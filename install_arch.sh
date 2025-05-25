d4a02cc6009bce2afaeea876e913b966375988b5
Eric Borgsten <eric.borgsten@gmail.com>
2025-05-25 17:07:44 +0200

    Arch dependencies script

---
+++ install_arch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+packages=(
+    # System
+    ghostty
+    ttf-jetbrains-mono-nerd
+    udiskie
+    brightnessctl
+    # WM Hyprland
+    deskflow
+    hypridle
+    hyprland
+    hyprlock
+    hyprpaper
+    swaync
+    waybar
+    wl-clipboard
+    # Dev
+    cmake
+    # Neovim
+    nvim
+    lua51
+    luarocks
+    # Util
+    bat
+    fd
+    fzf
+    less
+    ripgrep
+    tmux
+    unzip
+)
+
+sudo pacman -S --needed ${packages[@]}
