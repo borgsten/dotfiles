ff3030a17519ef39599259acd7303bf4d736ac65
Eric Borgsten <eric.borgsten@gmail.com>
2025-05-25 17:06:05 +0200

    Update nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,26 @@
 return {
   {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      spec = {
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>f", group = "[F]ind" },
+        { "<leader>g", group = "[G]it" },
+        { "<leader>h", group = "Git [H]unk" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>w", group = "[W]orkspace" },
+        { "<leader>x", group = "E[X]ecute" },
+        -- register which-key VISUAL mode
+        -- required for visual <leader>hs (hunk stage) to work
+        { "<leader>",  group = "VISUAL <leader>", mode = "v" },
+        { "<leader>h", desc = "Git [H]unk",       mode = "v" },
+      }
+    },
+  },
+  {
     'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
@@ -30,30 +51,7 @@ return {
       require("neo-tree").setup({
         close_if_last_window = true
       });
-      vim.keymap.set('n', '<leader>e', ":Neotree reveal toggle=true<CR>", { desc = "Toggle neotree" });
-    end
-  },
-  {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    config = function()
-      require('which-key').setup()
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-      -- register which-key VISUAL mode
-      -- required for visual <leader>hs (hunk stage) to work
-      require('which-key').register({
-        ['<leader>'] = { name = 'VISUAL <leader>' },
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add({ '<leader>e', mode = 'n', ":Neotree reveal toggle=true<CR>", desc = "Toggle neotree" })
     end
   },
 }
