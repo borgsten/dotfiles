8a8290e39c20c3f134a5f5117838054e56e50874
Eric Borgsten <eric.borgsten@gmail.com>
2025-05-25 17:07:26 +0200

    Nvim: Install hardtime

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'bash', 'markdown', 'markdown_inline', 'css', 'make', 'cmake', 'rust', 'vimdoc', 'yaml', 'json', 'luadoc', 'query' },
+      ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'bash', 'markdown', 'markdown_inline', 'css', 'make', 'cmake', 'rust', 'vimdoc', 'yaml', 'json', 'luadoc', 'query', 'diff' },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
