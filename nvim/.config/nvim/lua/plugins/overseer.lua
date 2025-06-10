46580f794d8be25af8386398a1a25bab0f1b4c0a
Eric Borgsten <eric.borgsten@gmail.com>
2025-06-11 00:28:38 +0200

    Add overseer.nvim

---
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,19 @@
+local function rerun_last_task()
+  local overseer = require("overseer")
+  local tasks = overseer.list_tasks({ recent_first = true })
+  if vim.tbl_isempty(tasks) then
+    vim.notify("No tasks found", vim.log.levels.WARN)
+  else
+    overseer.run_action(tasks[1], "restart")
+  end
+end
+
+return {
+  'stevearc/overseer.nvim',
+  opts = {},
+  keys = {
+    { "<leader>os", "<cmd>OverseerToggle!<cr>" },
+    { "<leader>or", rerun_last_task },
+    { "<leader>ot", "<cmd>OverseerRun<cr>" },
+  }
+}
