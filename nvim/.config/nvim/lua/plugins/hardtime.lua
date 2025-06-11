5382db3a27e1f506ac2291749a4d3ecadc9bedd2
Eric Borgsten <eric.borgsten@gmail.com>
2025-06-11 22:52:17 +0200

    nvim: enable arrows in insert

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,12 @@ return {
   "m4xshen/hardtime.nvim",
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim", "folke/snacks.nvim" },
-  opts = {},
+  opts = {
+    disabled_keys = {
+      ["<Up>"] = { "n" },
+      ["<Down>"] = { "n" },
+      ["<Left>"] = { "n" },
+      ["<Right>"] = { "n" },
+    },
+  },
 }
