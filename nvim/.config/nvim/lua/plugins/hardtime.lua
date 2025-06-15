d44cdd4082d4ce15b65a31ecb73cb835c9ac3abd
Eric Borgsten <eric.borgsten@gmail.com>
2025-06-15 22:55:41 +0200

    nvim: hardtime exceptions

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -3,11 +3,16 @@ return {
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim", "folke/snacks.nvim" },
   opts = {
+    disable_mouse = true,
     disabled_keys = {
       ["<Up>"] = { "n" },
       ["<Down>"] = { "n" },
       ["<Left>"] = { "n" },
       ["<Right>"] = { "n" },
+    },
+    restricted_keys = {
+      ["j"] = false,
+      ["k"] = false,
     },
   },
 }
