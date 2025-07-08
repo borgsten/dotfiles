063e20fc6e63e070dec5a7ac9b262c429763c49e
Eric Borgsten <eric.borgsten@gmail.com>
2025-07-08 02:11:53 +0200

    zsh: Improve completion

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -3,6 +3,12 @@
 autoload -Uz compinit && compinit
 _comp_options+=(globdots)
 
+unsetopt menu_complete   # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt auto_menu         # show completion menu on successive tab press
+setopt complete_in_word
+setopt always_to_end
+
 # Use cache for commands using cache
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
@@ -10,11 +16,17 @@ zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 zstyle ':completion:*' complete true
 
 zstyle ':completion:*' menu select
-zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' special-dirs false
+
+# squash // to /
+zstyle ':completion:*' squeeze-slashes true
 
 zstyle ':completion:*' complete-options true
 
 # case insensitive completion
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|=*' 'l:|=* r:|=*'
+
+# Colors
+zstyle ':completion:*' list-colors ''
 
 autoload -U +X bashcompinit && bashcompinit
