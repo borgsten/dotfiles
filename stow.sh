0ac9e8caceefbb8e76a2f0bf239cfe05ae0aa6b5
Eric Borgsten <eric.borgsten@gmail.com>
2025-08-18 13:55:32 +0200

    theming: do not track current theme symlink

--- stow.sh
+++ stow.sh
@@ -18,3 +18,7 @@ for config in "${configs[@]}"; do
 	echo "Stowing $config"
 	stow -R --target="${HOME}" "$config"
 done
+
+# Ignore the current theme symlink
+echo "Make git ignore current theme symlink"
+git update-index --assume-unchanged theming/.config/theming/current
