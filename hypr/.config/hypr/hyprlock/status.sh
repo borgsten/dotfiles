0b25261dbc2c274f318b8993c25b31f834a9bd8e
Eric Borgsten <eric.borgsten@gmail.com>
2025-10-05 01:25:03 +0200

    hypr: show battery on lockscreen

--- hypr/.config/hypr/hyprlock/status.sh
+++ hypr/.config/hypr/hyprlock/status.sh
@@ -2,6 +2,12 @@
 
 MAIN_KB_CAPS=$(hyprctl devices | grep -B 6 "main: yes" | grep "capsLock" | head -1 | awk '{print $2}')
 
+capacity_file="/sys/class/power_supply/BAT0/capacity"
+if [[ -f ${capacity_file} ]]; then
+    level=$(cat ${capacity_file})
+    echo "󱊣 ${level}%"
+fi
+
 if [ "$MAIN_KB_CAPS" = "yes" ]; then
     echo "⚠ Caps Lock active"
 else
