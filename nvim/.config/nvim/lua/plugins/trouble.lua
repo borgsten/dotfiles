224e27f44da6bc12f5858fef9fe85c0467c289f5
Eric Borgsten <eric.borgsten@gmail.com>
2025-11-18 17:17:34 +0100

    keybinding for trouble

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -2,4 +2,11 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
+  keys = {
+    {
+      "<leader>tt",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "[T]oggle [T]rouble Diagnostics",
+    },
+  }
 }
