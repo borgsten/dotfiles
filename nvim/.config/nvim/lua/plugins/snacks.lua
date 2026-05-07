887138b80a746e189b7eb57180d3c5e4a3f567b8
Eric Borgsten <eric.borgsten@gmail.com>
2026-05-07 12:40:57 +0200

    nvim: show version in dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
       enabled = true,
       notify = false
     },
+    ---@class snacks.dashboard.Config
     dashboard = {
       enabled = true,
       preset = {
@@ -23,6 +24,7 @@ return {
         { section = "header" },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { title = tostring(vim.version()), align = "center", padding = 1 },
         { section = "startup" },
       },
     },
