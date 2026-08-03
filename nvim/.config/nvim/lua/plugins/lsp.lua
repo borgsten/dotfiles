ee8d2c63d60c5b18523ad623ad3b903c9c0c3146
Eric Borgsten <eric.borgsten@gmail.com>
2026-08-03 12:24:25 +0200

    nvim: update plugins

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,16 @@
 local function switch_source_header_splitcmd(bufnr, splitcmd)
   local method_name = 'textDocument/switchSourceHeader'
   local client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
-  if not client then
+  ---
+  ---@diagnostic disable-next-line:param-type-mismatch
+  if not client or not client:supports_method(method_name) then
     return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
   end
 
   local params = vim.lsp.util.make_text_document_params(bufnr)
-  client.request(method_name, params, function(err, result)
+
+  ---@diagnostic disable-next-line:param-type-mismatch
+  client:request(method_name, params, function(err, result)
     if err then
       error(tostring(err))
     end
@@ -106,7 +110,7 @@ return {
       })
 
       -- LSP log level
-      vim.lsp.set_log_level(vim.log.levels.WARN)
+      vim.lsp.log.set_level(vim.log.levels.WARN)
 
       vim.diagnostic.config {
         severity_sort = true,
