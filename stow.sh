80cd5f51486ad427376cf79491be62b0addfcaee
Eric Borgsten <eric.borgsten@gmail.com>
2026-09-24 21:51:21 +0200

    do not track btop

--- stow.sh
+++ stow.sh
@@ -17,7 +17,6 @@ configs=(
     "ghostty"
     "services"
     "applications"
-    "btop"
     "noctalia"
     "wezterm"
 )
@@ -26,6 +25,17 @@ configs=(
 mkdir -p ~/.local/share/icons/Adwaita-Symbolic
 mkdir -p ~/.config/btop/themes
 ln -sf ../../../.cache/theming/btop.theme ~/.config/btop/themes/matugen.theme
+
+# btop messes with config in runtime, just set theme
+btop_conf=~/.config/btop/btop.conf
+# left over from when the config was stowed
+[[ -L $btop_conf ]] && rm "$btop_conf"
+# btop fills in everything else on launch
+if grep -q '^color_theme' "$btop_conf" 2>/dev/null; then
+	sed -i 's/^color_theme = .*/color_theme = "matugen"/' "$btop_conf"
+else
+	echo 'color_theme = "matugen"' >> "$btop_conf"
+fi
 
 for config in "${configs[@]}"; do
 	echo "Stowing $config"
