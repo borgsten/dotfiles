7e98f3599612da0314222c00a8d11a02edc21006
Eric Borgsten <eric.borgsten@gmail.com>
2026-09-24 12:12:18 +0200

    remove stale theme stuff

--- stow.sh
+++ stow.sh
@@ -31,9 +31,3 @@ for config in "${configs[@]}"; do
 	echo "Stowing $config"
 	stow -R --target="${HOME}" "$config"
 done
-
-# Ignore the current theme symlink
-if git rev-parse --is-inside-work-tree &>/dev/null; then
-    echo "Make git ignore current theme symlink"
-    git update-index --assume-unchanged theming/.config/theming/current
-fi
