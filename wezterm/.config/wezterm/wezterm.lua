07439ce666141e4abb30d0659502d664da3a7b9d
Eric Borgsten <eric.borgsten@gmail.com>
2026-09-24 18:26:39 +0200

    fix wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,6 +11,8 @@ config.color_scheme = 'Noctalia'
 config.window_background_opacity = 0.8
 config.text_background_opacity = 0.8
 
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
 -- tmux-style tab bar, coloured from config.color_scheme
 require('tabbar').apply_to_config(config)
 
